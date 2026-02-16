ee5c87b7a8ba9a48bd8824106b7ddaafda162fbe
Vergara Urias Sophia <sotivergaraur@ittepic.edu.mx>
2026-02-16 11:47:11 -0700

    feat: implememnta la clase Vehiculo

---
+++ Proyecto-VentaVehiculo-5B/src/Vehiculo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class Vehiculo {
+    
+    private String marca;
+    private double precio;
+    private String motor;
+
+    // Constructor
+    public Vehiculo(String marca, double precio, String motor) {
+        this.marca = marca;
+        this.precio = precio;
+        this.motor = motor;
+    }
+}
