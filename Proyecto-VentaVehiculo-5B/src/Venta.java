65b96bda299efbce488fadfbd21dc9f3263f9e43
Vergara Urias Sophia <sotivergaraur@ittepic.edu.mx>
2026-02-16 11:46:25 -0700

    feat: implememnta la clase Venta

---
+++ Proyecto-VentaVehiculo-5B/src/Venta.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class Venta {
+    
+    private int cantidad;
+    private Vehiculo vehiculo;
+    private Cliente cliente;
+
+    // Constructor
+    public Venta(int cantidad, Vehiculo vehiculo, Cliente cliente) {
+        this.cantidad = cantidad;
+        this.vehiculo = vehiculo;
+        this.cliente = cliente;
+    }
+ }
+
